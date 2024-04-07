9d12332bac04282fff61e130c15fddcfeeeb623c
Carmeldj <carmeldjegui01@gmail.com>
2024-04-08 00:51:58 +0100

    Start the ui of rock paper scissors and javascript part
    I finish the logic part I have to chamge the ui to be more cool

--- script.js
+++ script.js
@@ -5,35 +5,120 @@ function getComputerChoice() {
   else return "scissors";
 }
 
-//console.log(getComputerChoice());
+//function getPlayerSelection() {
+//const pl = prompt("Choose between rock paper and scissors:");
+//return pl.toLowerCase();
+//}
 
-function getPlayerSelection() {
-  const pl = prompt("Choose between rock paper and scissors:");
-  return pl.toLowerCase();
+function playRound(p) {
+  const c = getComputerChoice();
+  if (c == p) return "You are tie !!!";
+  else if (c == "rock" && p == "paper") return "You win! Paper beats Rock";
+  else if (c == "rock" && p == "scissors")
+    return "You lose! Rock beats Scissors";
+  else if (c == "scissors" && p == "paper")
+    return "You lose! Scissors beats Paper";
+  else if (c == "scissors" && p == "rock")
+    return "You win! Rock beats Scissors";
+  else if (c == "paper" && p == "rock") return "You lose! Paper beats rock";
+  else return "You win! Scissors beats Paper";
 }
 
-//console.log(playerSelection());
+const btn1 = document.querySelector(".bt1");
+const btn2 = document.querySelector(".bt2");
+const btn3 = document.querySelector(".bt3");
+const div = document.querySelector(".result");
+const res = document.querySelector(".para");
+let PlWins = 0;
+let CpWins = 0;
 
-function playGame() {
-  for (let i = 0; i <= 4; i++) {
-    const c = getComputerChoice();
-    const p = getPlayerSelection();
-
-    function playRound(c, p) {
-      if (c == p) return "You are tie !!!";
-      else if (c == "rock" && p == "paper") return "You win! Paper beats Rock";
-      else if (c == "rock" && p == "scissors")
-        return "You lose! Rock beats Scissors";
-      else if (c == "scissors" && p == "paper")
-        return "You lose! Scissors beats Paper";
-      else if (c == "scissors" && p == "rock")
-        return "You win! Rock beats Scissors";
-      else if (c == "paper" && p == "rock") return "You lose! Paper beats rock";
-      else return "You win! Scissors beats Paper";
+document.addEventListener("click", function (event) {
+  // check which link was clicked using the event.target.id property
+  if (PlWins < 5 && CpWins < 5) {
+    let round;
+    switch (event.target.className) {
+      case "bt1":
+        round = playRound("rock");
+        if (round.includes("win")) {
+          PlWins += 1;
+        } else if (round.includes("lose")) {
+          CpWins += 1;
+        }
+        div.textContent = round + " " + PlWins + " : " + CpWins;
+        break;
+      case "bt2":
+        round = playRound("paper");
+        if (round.includes("win")) {
+          PlWins += 1;
+        } else if (round.includes("lose")) {
+          CpWins += 1;
+        }
+        div.textContent = round + " " + PlWins + " : " + CpWins;
+        break;
+      case "bt3":
+        round = playRound("scissors");
+        if (round.includes("win")) {
+          PlWins += 1;
+        } else if (round.includes("lose")) {
+          CpWins += 1;
+        }
+        div.textContent = round + " " + PlWins + " : " + CpWins;
+        break;
+      default:
+        break;
     }
-
-    console.log(playRound(c, p));
+  } else if (PlWins == 5) {
+    res.textContent = "You win this game";
+    // console.log("you Win the game");
+    PlWins = 0;
+    CpWins = 0;
+    //console.log("Wanna play another game ?");
+  } else if (CpWins == 5) {
+    res.textContent = "You lose this game";
+    //console.log("you Lose the game");
+    PlWins = 0;
+    CpWins = 0;
+    //console.log("Wanna play another game ?");
   }
-}
+});
+res.textContent = "";
 
-playGame();
+//btn1.addEventListener("click", function () {
+/*p = "rock";
+    const rs = playRound(p);
+    div.textContent = rs;
+    if (rs.includes("win")) {
+      winP = winP + 1;
+    } else if (rs.includes("lose")) {
+      winC = winC + 1;
+    }
+    res.textContent = winP + " " + winC;
+    console.log(winC);
+    console.log(winP);
+});*/
+/*btn2.addEventListener("click", function () {
+  p = "paper";
+  const rs = playRound(p);
+  div.textContent = rs;
+  if (rs.includes("win")) {
+    winP = winP + 1;
+  } else if (rs.includes("lose")) {
+    winC = winC + 1;
+  }
+  res.textContent = winP + " " + winC;
+  console.log(winC);
+  console.log(winP);
+});
+btn3.addEventListener("click", function () {
+  p = "scissors";
+  const rs = playRound(p);
+  div.textContent = rs;
+  if (rs.includes("win")) {
+    winP = winP + 1;
+  } else if (rs.includes("lose")) {
+    winC = winC + 1;
+  }
+  res.textContent = winP + " " + winC;
+  console.log(winC);
+  console.log(winP);
+});*/
